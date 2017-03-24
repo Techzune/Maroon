ea1f9ef4f1ce15278e1f477cceeb325c842241c0
techzune <techzune@operontech.com>
2017-03-24 15:13:44 -0500

    Added home screen, and other minor changes

--- app/src/main/java/com/operontech/maroon/ActivityMain.java
+++ app/src/main/java/com/operontech/maroon/ActivityMain.java
@@ -2,9 +2,9 @@ package com.operontech.maroon;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,21 +12,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import com.operontech.maroon.frag.FragmentHome;
 
 public class ActivityMain extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
 	public final static String MAROONTAG = "Maroon";
 	private boolean backButtonExitTriggered = false;
+	FragmentManager fManager;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
+		// Set up the main content (fragment)
+		fManager = getSupportFragmentManager();
+		fManager.beginTransaction().add(R.id.main_fragment, new FragmentHome()).commit();
+
 		setContentView(R.layout.activity_main);
-		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+		// Set up the toolbar
+		Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
 		setSupportActionBar(toolbar);
 
-		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.main_fab);
 		fab.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
@@ -34,18 +43,20 @@ public class ActivityMain extends AppCompatActivity implements NavigationView.On
 			}
 		});
 
-		DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+		// Set up the navigation drawer
+		DrawerLayout drawer = (DrawerLayout) findViewById(R.id.main_drawerLayout);
 		ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
 		drawer.setDrawerListener(toggle);
 		toggle.syncState();
 
+		// Set up navigation view listener
 		NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
 		navigationView.setNavigationItemSelectedListener(this);
 	}
 
 	@Override
 	public void onBackPressed() {
-		DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+		DrawerLayout drawer = (DrawerLayout) findViewById(R.id.main_drawerLayout);
 		if (drawer.isDrawerOpen(GravityCompat.START)) {
 			drawer.closeDrawer(GravityCompat.START);
 		} else {
@@ -95,7 +106,7 @@ public class ActivityMain extends AppCompatActivity implements NavigationView.On
 
 		}
 
-		DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+		DrawerLayout drawer = (DrawerLayout) findViewById(R.id.main_drawerLayout);
 		drawer.closeDrawer(GravityCompat.START);
 		return true;
 	}
--- app/src/main/java/com/operontech/maroon/ActivityStartLast.java
+++ app/src/main/java/com/operontech/maroon/ActivityStartLast.java
@@ -1,14 +1,29 @@
 package com.operontech.maroon;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityStartLast extends AppCompatActivity {
+
+	Button finishButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_start_last);
+
+		finishButton = (Button) findViewById(R.id.startLast_button_begin);
+		finishButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Intent intent = new Intent(ActivityStartLast.this, ActivityMain.class);
+				startActivity(intent);
+				overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+			}
+		});
 	}
 
 	@Override
