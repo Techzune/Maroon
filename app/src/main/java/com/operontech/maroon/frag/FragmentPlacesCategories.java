bda639177af7be3794549b55555709ec08b08a31
techzune <techzune@operontech.com>
2017-05-08 10:46:18 -0500

    Created GridView for Places Categories

--- app/src/main/java/com/operontech/maroon/frag/FragmentPlacesCategories.java
+++ app/src/main/java/com/operontech/maroon/frag/FragmentPlacesCategories.java
@@ -1,4 +1,25 @@
 package com.operontech.maroon.frag;
 
-public class FragmentPlacesCategories {
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+import com.operontech.maroon.R;
+
+public class FragmentPlacesCategories extends Fragment {
+
+	GridView gridView;
+
+	@Nullable
+	@Override
+	public View onCreateView(final LayoutInflater inflater, @Nullable final ViewGroup container, final Bundle savedInstanceState) {
+		final View view = inflater.inflate(R.layout.fragment_places_categories, container, false);
+
+		gridView = (GridView) view.findViewById(R.id.places_categories_gridview);
+
+		return view;
+	}
 }
