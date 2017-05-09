a0eefdb29d162a05d89f83716c65e7a5ccfe2692
techzune <techzune@operontech.com>
2017-05-09 14:43:27 -0500

    Layout development for places list

--- app/src/main/java/com/operontech/maroon/frag/FragmentPlacesList.java
+++ app/src/main/java/com/operontech/maroon/frag/FragmentPlacesList.java
@@ -1,4 +1,16 @@
 package com.operontech.maroon.frag;
 
-public class FragmentPlacesList {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentPlacesList extends Fragment {
+	@Nullable
+	@Override
+	public View onCreateView(final LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable final Bundle savedInstanceState) {
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
 }
