6aa69175eff534d3e2c8c09336f63a10911830f4
Techzune <techzune@operontech.com>
2017-06-18 14:18:40 -0500

    Converted to Kotlin

---
+++ app/src/main/java/com/operontech/maroon/db/PlaceListing.kt
@@ -0,0 +1,3 @@
+package com.operontech.maroon.db
+
+data class PlaceListing(val id: String, val title: String, val description: String)
---
+++ app/src/main/java/com/operontech/maroon/db/PlaceType.kt
@@ -0,0 +1,14 @@
+package com.operontech.maroon.db
+
+import com.operontech.maroon.R
+
+enum class PlaceType(val title: String, val icon: Int) {
+    //@formatter:off
+	ACADEMIC("Academic", R.drawable.ic_academic),
+    ATHLETIC("Athletic", R.drawable.ic_athletic),
+    FOODANDDINING("Food & Dining", R.drawable.ic_dining),
+    RESIDENTHALL("Residence Hall", R.drawable.ic_action_home),
+    OTHER("Other", R.drawable.ic_help_outline),
+    ALL("All", R.drawable.ic_all);
+    //@formatter:on
+}
