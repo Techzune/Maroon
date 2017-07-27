68ecffe888885a0a8cc0a95bb34b306e5c826b60
Techzune <techzune@operontech.com>
2017-07-26 19:01:01 -0500

    Started next page for student info

---
+++ app/src/main/java/com/operontech/maroon/activity/ActivityStartStudentInfo.kt
@@ -0,0 +1,13 @@
+package com.operontech.maroon.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.operontech.maroon.R
+
+class ActivityStartStudentInfo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_studentinfo)
+    }
+}
