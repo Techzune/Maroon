1b0b8e0192e76911eefdcc378c379f25f29c88a2
Techzune <techzune@operontech.com>
2017-08-18 21:01:28 -0500

    Implemented direction from F..PlacesType to F..PlacesList

--- app/src/main/java/com/operontech/maroon/frag/FragmentPlacesTypes.kt
+++ app/src/main/java/com/operontech/maroon/frag/FragmentPlacesTypes.kt
@@ -5,11 +5,14 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.GridView
 import butterknife.BindView
 import butterknife.ButterKnife
 import com.operontech.maroon.R
+import com.operontech.maroon.activity.ActivityMain
 import com.operontech.maroon.adapter.GridViewAdapterPlacesType
+import com.operontech.maroon.db.PlaceType
 
 class FragmentPlacesTypes : Fragment() {
 
@@ -20,6 +23,15 @@ class FragmentPlacesTypes : Fragment() {
         val view = inflater!!.inflate(R.layout.fragment_places_types, container, false)
         ButterKnife.bind(this, view)
         gridView.adapter = GridViewAdapterPlacesType(activity)
+        gridView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+            val item = gridView.getItemAtPosition(position) as PlaceType
+            val bundle = Bundle()
+            bundle.putString("type", item.title)
+
+            val listFragment = FragmentPlacesList()
+            listFragment.arguments = bundle
+            (activity as ActivityMain).showFragment(listFragment)
+        }
         return view
     }
 
