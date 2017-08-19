9c4e0d9ebade847a0d5f6dc4208f4e0e574cf52d
Techzune <techzune@operontech.com>
2017-08-19 16:50:10 -0500

    Created GridViewAdapter for F..PlacesList and implemented placeholder

---
+++ app/src/main/java/com/operontech/maroon/adapter/GridViewAdapterPlacesList.kt
@@ -0,0 +1,30 @@
+package com.operontech.maroon.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.operontech.maroon.R
+import com.operontech.maroon.db.PlaceListing
+
+
+class GridViewAdapterPlacesList(private val placeListings: Array<PlaceListing>) : RecyclerView.Adapter<GridViewAdapterPlacesList.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent!!.context).inflate(R.layout.item_recyclerview_placelisting, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return placeListings.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+        val listingName = placeListings[position].title
+        holder!!.placeName.text = listingName
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val placeName: TextView = view.findViewById(R.id.placelisting_text_title)
+    }
+}
--- app/src/main/java/com/operontech/maroon/frag/FragmentPlacesList.kt
+++ app/src/main/java/com/operontech/maroon/frag/FragmentPlacesList.kt
@@ -2,18 +2,34 @@ package com.operontech.maroon.frag
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import butterknife.BindView
+import butterknife.ButterKnife
 import com.operontech.maroon.R
+import com.operontech.maroon.adapter.GridViewAdapterPlacesList
+import com.operontech.maroon.db.PlaceListing
 
 class FragmentPlacesList : Fragment() {
+
+    @BindView(R.id.places_list_recyclerview)
+    lateinit var recyclerView: RecyclerView
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        val view = inflater!!.inflate(R.layout.fragment_places_list, container, false)
+        ButterKnife.bind(this, view)
+        recyclerView.adapter = GridViewAdapterPlacesList(getList())
+        return view
     }
 
     override fun onResume() {
         super.onResume()
         activity.title = getString(R.string.title_placesList, arguments.getString("type"))
+    }
+
+    private fun getList(): Array<PlaceListing> {
+        return Array(10, { PlaceListing("0", "test", "test") })
     }
 }
