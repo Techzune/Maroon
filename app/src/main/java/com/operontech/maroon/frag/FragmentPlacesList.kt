4459feea8689af87fae0695629f4f2bb7f97b24b
Techzune <techzune@operontech.com>
2017-08-18 21:12:12 -0500

    Created parameter string title for F..PlacesList

--- app/src/main/java/com/operontech/maroon/frag/FragmentPlacesList.kt
+++ app/src/main/java/com/operontech/maroon/frag/FragmentPlacesList.kt
@@ -5,9 +5,15 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.operontech.maroon.R
 
 class FragmentPlacesList : Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        activity.title = getString(R.string.title_placesList, arguments.getString("type"))
     }
 }
