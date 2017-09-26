31b88ff9fd70bd54d35f979353f04db85b7d0d46
techzune <techzune@operontech.com>
2017-09-26 14:34:22 -0500

    Replaced the icons on the "Categories" page with vectors for resizing

--- app/src/main/java/com/operontech/maroon/db/PlaceType.kt
+++ app/src/main/java/com/operontech/maroon/db/PlaceType.kt
@@ -4,11 +4,11 @@ import com.operontech.maroon.R
 
 enum class PlaceType(val title: String, val icon: Int) {
     //@formatter:off
-	ACADEMIC("Academic", R.drawable.ic_academic),
-    ATHLETIC("Athletic", R.drawable.ic_athletic),
-    FOODANDDINING("Food & Dining", R.drawable.ic_dining),
-    RESIDENCEHALL("Residence Hall", R.drawable.ic_action_home),
+	ACADEMIC("Academic", R.drawable.ic_school),
+    ATHLETIC("Athletic", R.drawable.ic_fitness_center),
+    FOODANDDINING("Food & Dining", R.drawable.ic_local_dining),
+    RESIDENCEHALL("Residence Hall", R.drawable.ic_home),
     OTHER("Other", R.drawable.ic_help_outline),
-    ALL("All", R.drawable.ic_all);
+    ALL("All", R.drawable.ic_all_inclusive);
     //@formatter:on
 }
