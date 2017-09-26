7115a60a7c13b33d1dda3ad20489ec2ae885d690
Techzune <techzune@operontech.com>
2017-09-25 22:30:02 -0500

    Fixed RESIDENCEHALL naming error

--- app/src/main/java/com/operontech/maroon/db/PlaceType.kt
+++ app/src/main/java/com/operontech/maroon/db/PlaceType.kt
@@ -7,7 +7,7 @@ enum class PlaceType(val title: String, val icon: Int) {
 	ACADEMIC("Academic", R.drawable.ic_academic),
     ATHLETIC("Athletic", R.drawable.ic_athletic),
     FOODANDDINING("Food & Dining", R.drawable.ic_dining),
-    RESIDENTHALL("Residence Hall", R.drawable.ic_action_home),
+    RESIDENCEHALL("Residence Hall", R.drawable.ic_action_home),
     OTHER("Other", R.drawable.ic_help_outline),
     ALL("All", R.drawable.ic_all);
     //@formatter:on
